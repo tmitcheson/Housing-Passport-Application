62979b7d5786604c7fb88eafdb57068ae8885795
tmitcheson <tbmitcheson@gmail.com>
2022-08-02 08:54:59 +0100

    remove validator references

--- housing-passport-back-end/app.py
+++ housing-passport-back-end/app.py
@@ -8,16 +8,6 @@ from findDataFromLMK import findDataFromLMK
 from addEPCDataToUser import addEPCDataToUser
 from getMyProperty import getMyProperty
 
-from authlib.integrations.flask_oauth2 import ResourceProtector
-from validator import Auth0JWTBearerTokenValidator
-
-require_auth = ResourceProtector()
-validator = Auth0JWTBearerTokenValidator(
-    "dev-5g0j9i2z.us.auth0.com",
-    "https://housing-passport-backend.com"
-)
-require_auth.register_token_validator(validator)
-
 app = Flask(__name__)
 
 CORS(app)
@@ -169,7 +159,6 @@ def get_list_of_tradespeople():
 
 
 @app.route("/api/private", methods=["GET"])
-@require_auth(None)
 def private():
     print("hllo")
     response = "Hello from a private endpoint! You need to be authenticated to see this."
