e1fd0fea8350a1a59b97ff6d152cd6a0af90bfbc
tmitcheson <tbmitcheson@gmail.com>
2022-08-18 15:38:30 +0100

    can see public updates from the logged out section

--- front-end/pages/property/[lmk].js
+++ front-end/pages/property/[lmk].js
@@ -3,11 +3,16 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import BasicTabs from "../../components/EpcTabs";
 import { useState } from "react";
+import { set } from "react-hook-form";
+import styles from "../../styles/List.module.css";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
 
 const propertyDetails = () => {
   const router = useRouter();
   const [epcReady, setEpcReady] = useState(false);
   const [chosenProperty, setChosenProperty] = useState({});
+  const [retrofitsPresent, setRetrofitsPresent] = useState(false);
+  const [retrofits, setRetrofits] = useState();
 
   useEffect(() => {
     const lmk_key = router.query["lmk"];
@@ -18,19 +23,36 @@ const propertyDetails = () => {
         console.log(response.data);
         const epcData = response.data;
         setChosenProperty({ content: epcData });
+        if (epcData["retrofits"]) {
+          setRetrofitsPresent(true);
+          setRetrofits(epcData["retrofits"]);
+        }
         setEpcReady(true);
       });
   }, [router]);
 
   return (
-    <div>
-      {epcReady && (
-        <>
-          <h1> Showing data for {chosenProperty["content"]["ADDRESS"]}</h1>
-          <BasicTabs chosenProperty={chosenProperty} />
-        </>
-      )}
-    </div>
+    <>
+      <div>
+        {epcReady && (
+          <>
+            <h1> Showing data for {chosenProperty["content"]["ADDRESS"]}</h1>
+            <BasicTabs chosenProperty={chosenProperty} />
+          </>
+        )}
+      </div>
+      {retrofitsPresent &&
+        (<><div> Retrofits undertaken: </div>
+        {retrofits.map((item) => {
+          return (
+            <div className={styles.single} key={item}>
+              <CheckBoxIcon sx={{ color: "green" }}></CheckBoxIcon>
+              {"  "}
+              {item[0]}
+            </div>
+          );
+        })}</>)}
+    </>
   );
 };
 
